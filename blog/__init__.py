594fb1d442a92637c2e531c6f9ae47178143ef60
BarbodH <habibi.k.barbod@gmail.com>
2023-11-04 14:11:55 -0400

    Set up basic SQLite database

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from blog.home.views import home
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "8f1c53363ab158468a956baf"
+db = SQLAlchemy(app)
+app.app_context().push()
 
 # Register the blueprints
 app.register_blueprint(home, url_prefix="")
---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from blog import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
