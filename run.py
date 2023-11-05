4035f8fd415df59d0236aab18f7928dc045378c9
MoeHosseini <mmhossirca@gmail.com>
2023-11-04 23:45:20 -0400

    added features

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from blog.app_factory import create_app
+import nltk
 
 if __name__ == "__main__":
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
+    nltk.download('stopwords')
     app = create_app()
     app.run(debug=True)
